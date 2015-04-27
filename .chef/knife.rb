da93e33e7729c80a194973e99e8d4b0af6a6ab6c
hiroolie <hiroolie@gmail.com>
2015-04-28 01:38:22 +0900

    My first commit chef-repo
    Kickstarter

---
+++ .chef/knife.rb
@@ -0,0 +1,11 @@
+# See https://docs.getchef.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+validation_client_name   "czs-validator"
+validation_key           "#{current_dir}/czs-validator.pem"
+chef_server_url          "https://chef01.mini.com/organizations/czs"
+cookbook_path            ["#{current_dir}/../cookbooks"]
