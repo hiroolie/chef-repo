2dfe35d95ae2e1737fcae0b4951acfc9c128b23d
hiroolie <hiroolie@gmail.com>
2015-05-21 19:37:59 +0900

    Certs restore

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -3,8 +3,8 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "admin"
-client_key               "#{current_dir}/admin.pem"
+node_name                "hiroolie"
+client_key               "#{current_dir}/hiroolie.pem"
 validation_client_name   "czs-validator"
 validation_key           "#{current_dir}/czs-validator.pem"
 chef_server_url          "https://chef01.mini.com/organizations/czs"
